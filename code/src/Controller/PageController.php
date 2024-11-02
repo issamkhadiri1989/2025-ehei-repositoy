c511a3fb6ee7abd5dbaf1cbdae61fa1b7d509466
Issam KHADIRI <khadiri.issam@gmail.com>
2024-11-02 20:47:25 +0100

    TP 1 : prise en main des routes / twig en Symfony

---
+++ code/src/Controller/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class PageController extends AbstractController
+{
+    #[Route('/page', name: 'app_page')]
+    public function index(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+        ]);
+    }
+
+    #[Route('/contact-us', name: 'app_contact_us')]
+    public function contactUs(): Response
+    {
+        return $this->render('page/contact_us.html.twig');
+    }
+}
