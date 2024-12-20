6d8cddd97ed1ac7d4462bd6a28dcd09892757c43
Issam KHADIRI <khadiri.issam@gmail.com>
2024-12-20 21:29:20 +0100

    validate forms using constraints option per field

--- code/src/Form/Type/BlogType.php
+++ code/src/Form/Type/BlogType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class BlogType extends AbstractType
 {
@@ -21,12 +22,21 @@ class BlogType extends AbstractType
                 'placeholder' => 'Ipsum lorem dolore...',
             ],
             'help' => 'This title will be displayed in the home page. So it needs to be as clear as possible.',
+            'constraints' => [
+                new Assert\NotBlank(message: 'Ce champ ne peut pas être vide.'),
+                new Assert\NotNull(message: 'Ce champ est obligatoire.')
+            ],
         ])
             ->add('content', TextareaType::class, [
                 'label' => "Blog content",
                 'attr' => [
                     'rows' => 10,
                     'placeholder' => 'You can set any content here...',
+                ],
+                'help' => 'Le contenu du blog ne doit pas dépasser 200 caractères',
+                'constraints' => [
+                    new Assert\Length(max: 200, maxMessage: 'Ce contenu dépasse {{ limit }} caractères autorisés'),
+                    new Assert\NotNull(message: 'Le contenu du blog est obligatoire'),
                 ],
             ]);
     }
