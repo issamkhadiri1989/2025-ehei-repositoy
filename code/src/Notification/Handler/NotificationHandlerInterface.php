ea0cb4488c7ddf31087d45901ae2ed0f022b72a4
Issam KHADIRI <khadiri.issam@gmail.com>
2025-03-20 01:26:14 +0000

    tag all request handlers with the app.request_handler

--- code/src/Notification/Handler/NotificationHandlerInterface.php
+++ code/src/Notification/Handler/NotificationHandlerInterface.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace App\Notification\Handler;
 
+//use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+//use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
 use Symfony\Component\HttpFoundation\Request;
 
+//#[AutoconfigureTag(name: 'app.notification_handler')]
+//#[Autoconfigure(tags: ['app.notification_handler'])]
 interface NotificationHandlerInterface
 {
     public function handle(Request $request): void;
